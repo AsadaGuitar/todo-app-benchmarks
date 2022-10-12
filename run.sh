27aaad6bf01ab8af62fbf92d7232630622395da2
AsadaGuitar <asadaGuitar@gmail.com>
2022-10-12 22:44:51 +0900

    modified run.sh, added docker-compose exec rust-app cargo build

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 docker network create todo-app
 docker-compose up -d
+docker-compose exec rust-app cargo build
 docker-compose exec rust-app diesel migration run
 docker-compose exec rust-app cargo run -v
